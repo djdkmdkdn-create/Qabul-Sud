d3cb5eed46d11a229040798e8f5b5425a07ff0f9
Djdk <djdk@example.com>
2026-06-29 16:45:33 +0500

    select none

--- app/app.vue
+++ app/app.vue
@@ -5,7 +5,7 @@ import Navbar from './pages/navbar.vue';
 </script>
 <template>
   <Navbar></Navbar>
-  <div id="root">
+  <div class="select-none" id="root">
         
             <NuxtPage></NuxtPage>
                   </div>
